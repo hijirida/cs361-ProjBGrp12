3b39267dc22693c30c5f66f853aee8eb2bd3b590
Greg <gregf@ip-172-31-34-183.us-west-2.compute.internal>
2016-11-30 03:46:01 +0000

    Added individual progressbars to each charity, updated how donatorDashboard displays.

--- projectdb.sql
+++ projectdb.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS `donor`;
 CREATE TABLE `donor` (`donor_id` int NOT NULL AUTO_INCREMENT, `username` varchar(255), `password` varchar(255), `first_name` varchar(255), `last_name` varchar(255), `short_description` varchar(255), PRIMARY KEY(`donor_id`));
 CREATE TABLE `charity` (`charity_id` int NOT NULL AUTO_INCREMENT, `charity_name` varchar(255), `charity_website` varchar(255), `charity_description` varchar(255), PRIMARY KEY(`charity_id`));
 CREATE TABLE `sponsor` (`sponsor_id` int NOT NULL AUTO_INCREMENT, `sponsor_name` varchar(255), PRIMARY KEY(`sponsor_id`));
-CREATE TABLE `sponsorships` (`charity_id` int NOT NULL, `donation_amount` int, `sponsorship` int, `cur_steps` int, `sponsor_id` int, PRIMARY KEY(`charity_id`), FOREIGN KEY(`charity_id`) REFERENCES `charity` (`charity_id`), FOREIGN KEY(`sponsor_id`) REFERENCES `sponsor` (`sponsor_id`));
+CREATE TABLE `sponsorships` (`charity_id` int NOT NULL, `donation_amount` int, `sponsorship` int, `cur_steps` int, `progress` int, `sponsor_id` int, PRIMARY KEY(`charity_id`), FOREIGN KEY(`charity_id`) REFERENCES `charity` (`charity_id`), FOREIGN KEY(`sponsor_id`) REFERENCES `sponsor` (`sponsor_id`));
 CREATE TABLE `donor_sponsor` (`donor_id` int NOT NULL, `sponsorship_id` int, PRIMARY KEY(`donor_id`), FOREIGN KEY(`sponsorship_id`) REFERENCES `sponsorships` (`charity_id`), FOREIGN KEY(`donor_id`) REFERENCES `donor` (`donor_id`));
 
 INSERT INTO `sponsor` (`sponsor_name`) VALUES ('New Seasons');
@@ -19,7 +19,7 @@ INSERT INTO `donor` (`username`, `password`, `first_name`) VALUES ('Example User
 INSERT INTO `charity` (`charity_name`, `charity_website`, `charity_description`) VALUES ('Red Cross', 'www.redcross.org', 'Blood Donation Program');
 INSERT INTO `charity` (`charity_name`, `charity_website`, `charity_description`) VALUES ('Doctors without Borders', 'www.doctorswithoutborders.org', 'Providing medical aid where it is needed most');
 
-INSERT INTO `sponsorships` (`donation_amount`, `sponsorship`, `charity_id`, `sponsor_id`, `cur_steps`) VALUES (100, 10000, 1, 1, 5000);
-INSERT INTO `sponsorships` (`donation_amount`, `sponsorship`, `charity_id`, `sponsor_id`, `cur_steps`) VALUES (250, 50000, 2, 2, 15000);
+INSERT INTO `sponsorships` (`donation_amount`, `sponsorship`, `charity_id`, `sponsor_id`, `cur_steps`, `progress`) VALUES (100, 10000, 1, 1, 5000, 50);
+INSERT INTO `sponsorships` (`donation_amount`, `sponsorship`, `charity_id`, `sponsor_id`, `cur_steps`, `progress`) VALUES (250, 50000, 2, 2, 15000, 30);
 
 INSERT INTO `donor_sponsor` (`sponsorship_id`, `donor_id`) VALUES (1, 1);
